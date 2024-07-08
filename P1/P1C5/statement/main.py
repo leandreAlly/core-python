2ba85e44da6c1fa00b4f749f069b63ff862b291f
leandre <tuyambazeleandre@gmail.com>
2024-07-08 06:34:22 +0200

    learn basic types

--- P1/P1C5/statement/main.py
+++ P1/P1C5/statement/main.py
@@ -1,1 +1,18 @@
 # Write your code here!
+
+name = "leandre ally"
+age = 24
+height = 179.34
+is_student = True
+
+# Display the value of all variables
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height}")
+print(f"Is student: {is_student}")
+
+# Display the type of all variables
+print(f"Type name: {type(name)}")
+print(f"Type age: {type(age)}")
+print(f"Type height: {type(height)}")
+print(f"Type is student: {type(is_student)}")
