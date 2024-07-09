03f0643a879350e52b67313ace7b31637a0ec954
leandre <tuyambazeleandre@gmail.com>
2024-07-09 12:34:02 +0200

    Work on list

--- open-classroom/P1/P1C6/statement/main.py
+++ open-classroom/P1/P1C6/statement/main.py
@@ -1,1 +1,16 @@
 # Write your code here!
+
+fruits = ["apple", "banana", "orange"]
+
+fruits.append("kiwi")
+
+fruits.remove("orange")
+
+
+fruits[1] = "pineapple"
+
+print(len(fruits))
+
+fruits.sort()
+print(fruits)
+
