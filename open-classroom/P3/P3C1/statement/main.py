601e76140cf2f6abcccbd4bb9569e9115c0cb7ec
leandre <tuyambazeleandre@gmail.com>
2024-08-09 20:15:56 +0200

    learn import and use function from another file

--- open-classroom/P3/P3C1/statement/main.py
+++ open-classroom/P3/P3C1/statement/main.py
@@ -1,5 +1,13 @@
 # Write your code here!
+from operations import sum
+from operations import product
 
+def main():
+    calculated_sum = sum(3,5)
+    print("Result of the sum:",calculated_sum)
+    
+    calculated_product = product(8, 2)
+    print("Result of the product:",calculated_product)
 
 # Do not modify the code below
 if __name__ == "__main__":
--- open-classroom/P3/P3C1/statement/operations.py
+++ open-classroom/P3/P3C1/statement/operations.py
@@ -1,1 +1,6 @@
 # Write your code here!
+def sum(a,b):
+  return a + b
+
+def product(a, b):
+  return a * b
